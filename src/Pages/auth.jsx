1cb05ebe92716f9be3356fcfebc97e47604b7e68
sshivanshg <sshivanshg@gmail.com>
2024-09-13 18:07:58 +0530

    routing

--- src/Pages/auth.jsx
+++ src/Pages/auth.jsx
@@ -1,8 +1,36 @@
+import { LogIn } from 'lucide-react';
+import Login from "@/components/login";
+import Signup from "@/components/signup";
 import React from 'react'
+import { useSearchParams } from 'react-router-dom';
+import {Tabs, TabsContent, TabsList, TabsTrigger} from "@/components/ui/tabs";
+
 
 const Auth = () => {
+  const [SearchParams] = useSearchParams();
+
   return (
-    <div>auth</div>
+    <div className="mt-36 flex flex-col items-center gap-10">
+    <h1 className="text-5xl font-extrabold">
+    {SearchParams.get("CreateNew")
+    ? "Hold UP! lET'S LOGIN FIRST..."
+
+    : "LogIn/ SignUp"}
+    </h1>
+    <Tabs defaultValue="login" className="w-[400px]">
+        <TabsList className="grid w-full grid-cols-2">
+          <TabsTrigger value="login">Login</TabsTrigger>
+          <TabsTrigger value="signup">Signup</TabsTrigger>
+        </TabsList>
+        <TabsContent value="login">
+
+          <Login/>
+        </TabsContent>
+        <TabsContent value="signup">
+          <Signup />
+        </TabsContent>
+      </Tabs>
+    </div>
   )
 }
 
--- src/layouts/app-layouts.jsx
+++ src/layouts/app-layouts.jsx
@@ -9,7 +9,7 @@ const AppLayout = () => {
         <Outlet />
       </main>
       <div className="p-10 text-center bg-gray-800 mt-10">
-        Learning bit by bit
+        Building bit by bit
       </div>
     </div>
   );
