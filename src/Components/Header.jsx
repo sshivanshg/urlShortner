a805531c3eb643752b86be970bdc812cedbdf43d
sshivanshg <sshivanshg@gmail.com>
2024-09-13 05:43:43 +0530

    go

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,18 +1,46 @@
 import React from 'react'
 import { Link, useNavigate } from 'react-router-dom'
 import {Button} from "./ui/button";
+import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuLabel, DropdownMenuSeparator, DropdownMenuTrigger } from './ui/dropdown-menu';
+import { Avatar, AvatarImage } from '@radix-ui/react-avatar';
+import { AvatarFallback } from './ui/avatar';
+
 
 
 
 const Header = () => {
     const navigate =  useNavigate()
+    const user = true;
+
   return (
   <nav className='py-4 flex justify-between items-center'>
      <Link to="/">
         <img src="/logo.png" className="h-16" alt="Trimmer logo"/>
     </Link>
+
     <div>
+        {!user ?(
         <Button onClick={() => navigate("/auth")}>Login</Button>
+
+            ) : (
+        <DropdownMenu>
+        <DropdownMenuTrigger className="w-10 rounded-full overflow-hidden">
+        <Avatar>
+            <AvatarImage src="https://github.com/shadcn.png" />
+            <AvatarFallback>CN</AvatarFallback>
+        </Avatar>
+        </DropdownMenuTrigger>
+        <DropdownMenuContent>
+            <DropdownMenuLabel>My Account</DropdownMenuLabel>
+            <DropdownMenuSeparator />
+            <DropdownMenuItem>Profile</DropdownMenuItem>
+            <DropdownMenuItem>Billing</DropdownMenuItem>
+            <DropdownMenuItem>Team</DropdownMenuItem>
+            <DropdownMenuItem>Subscription</DropdownMenuItem>
+        </DropdownMenuContent>
+        </DropdownMenu>
+            )
+        }
     </div>
 </nav>
   )
