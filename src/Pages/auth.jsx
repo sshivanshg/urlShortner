8777c57dd5be3be403358871d0db8645c1e5d550
sshivanshg <sshivanshg@gmail.com>
2024-09-16 15:18:29 +0530

    Update auth.jsx

--- src/Pages/auth.jsx
+++ src/Pages/auth.jsx
@@ -1,36 +1,33 @@
-import Login from "@/components/login";
-import Signup from "@/components/signup";
-import React from 'react'
+import Login from "../Components/login.jsx";
+import Signup from "../Components/signup.jsx";
+import React from 'react';
 import { useSearchParams } from 'react-router-dom';
-import {Tabs, TabsContent, TabsList, TabsTrigger} from "@/components/ui/tabs";
-
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "../Components/ui/tabs";
 
 const Auth = () => {
-  const [SearchParams] = useSearchParams();
+  const [searchParams] = useSearchParams();
 
   return (
     <div className="mt-36 flex flex-col items-center gap-10">
-    <h1 className="text-5xl font-extrabold">
-    {SearchParams.get("CreateNew")
-    ? "Hold UP! lET'S LOGIN FIRST..."
-
-    : "LogIn/ SignUp"}
-    </h1>
-    <Tabs defaultValue="login" className="w-[400px]">
+      <h1 className="text-5xl font-extrabold">
+        {searchParams.get("CreateNew")
+          ? "Hold Up! Let's Login First..."
+          : "Login / Signup"}
+      </h1>
+      <Tabs defaultValue="login" className="w-[400px]">
         <TabsList className="grid w-full grid-cols-2">
           <TabsTrigger value="login">Login</TabsTrigger>
           <TabsTrigger value="signup">Signup</TabsTrigger>
         </TabsList>
         <TabsContent value="login">
-
-          <Login/>
+          <Login />
         </TabsContent>
         <TabsContent value="signup">
           <Signup />
         </TabsContent>
       </Tabs>
     </div>
-  )
-}
+  );
+};
 
 export default Auth;
